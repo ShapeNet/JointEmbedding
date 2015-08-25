831ecf59a58d83953a6a8d9f799347f0578104d0
Yangyan Li <yangyan@orionp.stanford.edu>
2015-08-25 12:10:48 -0700

    Small changes.

--- JointEmbedding/src/shape_embedding_training/extract_pyramid_hog.m
+++ JointEmbedding/src/shape_embedding_training/extract_pyramid_hog.m
@@ -1,5 +1,6 @@
 function [hog_feature] = extract_pyramid_hog(image_filename, hog_image_size)
-    I = imread(image_filename);
+    [I, map, alpha] = imread(filename);
+    I = rgb2gray(I)+(255-alpha*255);
     I = imresize(I, [hog_image_size, hog_image_size]);
     h0 = hog(single(I));
     I = imresize(I, 0.5);
