c418a1db8cb111e7a4ae34edfe4e131fe28b03bc
Yangyan Li <yangyan@oriong.stanford.edu>
2015-08-28 10:01:02 -0700

    Avoid use parpool size that is more than the system setting.

--- JointEmbedding/src/image_embedding_training/batch_crop_syn_images.m
+++ JointEmbedding/src/image_embedding_training/batch_crop_syn_images.m
@@ -2,7 +2,8 @@ addpath('../');
 
 global_variables;
 
-poolobj = parpool('local', g_syn_cropping_thread_num);
+local_cluster = parcluster('local');
+poolobj = parpool('local', min(g_syn_cropping_thread_num, local_cluster.NumWorkers));
 for i = 1:length(g_shapenet_synset_set)
     synset = g_shapenet_synset_set{i};
     fprintf('Batch cropping \"%s\" to \"%s\" ...\n', fullfile(g_syn_images_folder, synset), fullfile(g_syn_images_cropped_folder, synset));
