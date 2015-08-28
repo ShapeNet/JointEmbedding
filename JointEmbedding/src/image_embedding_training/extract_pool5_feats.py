4dc166649d42e00ef9c3db38e623b50439285cbc
Yangyan Li <yangyan@oriong.stanford.edu>
2015-08-27 20:57:38 -0700

    Fix path.

--- JointEmbedding/src/image_embedding_training/extract_pool5_feats.py
+++ JointEmbedding/src/image_embedding_training/extract_pool5_feats.py
@@ -7,7 +7,7 @@ BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 sys.path.append(os.path.dirname(BASE_DIR))
 from global_variables import *
 
-script_path = os.path.join(BASE_DIR, 'image_embedding_training/extract_feature_batch.py')
+script_path = os.path.join(BASE_DIR, 'extract_feature_batch.py')
 cmd = 'python %s --caffe_path %s --img_filelist %s --prototxt %s --caffemodel %s --feat_name pool5 --lmdb %s --gpu_index %d --pool_size %d' % \
 (script_path, g_caffe_install_path, g_syn_images_filelist, g_extract_feat_prototxt, g_fine_tune_caffemodel, g_pool5_lmdb, g_extract_feat_gpu_index, g_extract_feat_thread_num)
 
