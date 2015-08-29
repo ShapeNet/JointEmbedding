dcf3f6480e32fb7dc4e59c7c1e21059b34551b3c
Yangyan Li <yangyan@orionp.stanford.edu>
2015-08-29 14:58:47 -0700

    Reduce the memory required for generating Siamese lmdb.

--- JointEmbedding/src/siamese_embedding/gen_syn_images_pairs_lmdbs.py
+++ JointEmbedding/src/siamese_embedding/gen_syn_images_pairs_lmdbs.py
@@ -4,10 +4,11 @@
 import os
 import sys
 import lmdb
+import ctypes
 import shutil
 import datetime
 import numpy as np
-from multiprocessing import Pool
+import multiprocessing
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 sys.path.append(os.path.dirname(BASE_DIR))
@@ -24,54 +25,62 @@ except:
         print("Failed to include caffe_pb2, things might go wrong!")
     else:
         raise
-        
-def image_pair_to_string(pair_info):
+
+print datetime.datetime.now().time(), '- Allocating memory...'
+syn_images_num = len([line.strip() for line in open(g_syn_images_train_val_split, 'r')])
+env = lmdb.open(g_pool5_lmdb, readonly=True)
+datum_array = np.empty(0)
+with env.begin() as txn:
+    cursor = txn.cursor()
+    for key, value in cursor:
+        datum = caffe_pb2.Datum()
+        datum.ParseFromString(value)
+        datum_array = caffe.io.datum_to_array(datum)
+        break;
+shared_array_base = multiprocessing.Array(ctypes.c_double, syn_images_num*datum_array.size)
+shared_array = np.ctypeslib.as_array(shared_array_base.get_obj())
+shared_array = shared_array.reshape([syn_images_num]+list(datum_array.shape))
+
+def image_pair_to_string(pair_info, def_param=shared_array):
     image_pair = pair_info[0]
-    id_to_array = pair_info[1]
-    pair_idx = pair_info[2]
+    pair_idx = pair_info[1]
     array_1_id = image_pair[0]
     array_2_id = image_pair[1]
-    array_1 = id_to_array[array_1_id]
-    array_2 = id_to_array[array_2_id]
+    array_1 = shared_array[array_1_id, :]
+    array_2 = shared_array[array_2_id, :]
     
     array = np.concatenate((array_1, array_2), axis=0)
     datum = caffe.io.array_to_datum(array)
     datum.label = pair_idx
-    return datum.SerializeToString()  
+    return datum.SerializeToString() 
 
-def string_to_array(datum_string):
+def string_to_array(datum_string_idx, def_param=shared_array):
+    datum_string = datum_string_idx[0]
+    idx = datum_string_idx[1]
     datum = caffe_pb2.Datum()
     datum.ParseFromString(datum_string)
-    return caffe.io.datum_to_array(datum)
-    
-syn_images_num = len([line.strip() for line in open(g_syn_images_train_val_split, 'r')])
+    shared_array[idx, :] = caffe.io.datum_to_array(datum)
 
-pool = Pool(g_gen_siamese_lmdb_thread_num)
-#pool = Pool(1)  
+pool = multiprocessing.Pool(g_gen_siamese_lmdb_thread_num)
 report_step = 1000;
 
-print 'Loading feature from %s...'%(g_pool5_lmdb)
-id_to_array = dict()
-
-env = lmdb.open(g_pool5_lmdb, readonly=True)
-idx = 0
-cache_id = []
-cache_datum_string = []
+print  datetime.datetime.now().time(), '- Loading feature from %s...'%(g_pool5_lmdb)
+loaded_count = 0
+cache = []
 convert_count = 512
 with env.begin() as txn:
     cursor = txn.cursor()
     for key, value in cursor:
-        cache_id.append((int)(key))
-        cache_datum_string.append(value)
-        if (len(cache_datum_string) == convert_count or idx == syn_images_num-1):
-            array_list = pool.map(string_to_array, cache_datum_string)
-            id_to_array.update(dict(zip(cache_id, array_list)))
-            del cache_id[:]
-            del cache_datum_string[:]
-            
-        if(idx%report_step == 0):
-            print datetime.datetime.now().time(), '-', idx, 'of', syn_images_num, ' features loaded!'
-        idx = idx + 1
+        cache.append((value, (int)(key)))
+        if (len(cache) == convert_count or loaded_count == syn_images_num-1):
+            pool.map(string_to_array, cache)
+            del cache[:]
+        if(loaded_count%report_step == 0):
+            print datetime.datetime.now().time(), '-', loaded_count, 'of', syn_images_num, 'features loaded!'
+        loaded_count = loaded_count + 1
+env.close()
+
+print datetime.datetime.now().time(), '- Starting generating pairs lmdbs...'
 
 if os.path.exists(g_pairs_pool5_lmdb_train):
     shutil.rmtree(g_pairs_pool5_lmdb_train)
@@ -93,23 +102,23 @@ for idx, image_pair in enumerate(image_pair_list):
     key_idx = '{:0>10d}'.format(idx)
     if train_val_split[idx]:
         cache_train_id.append(key_idx)
-        cache_train_image_pair.append((image_pair, id_to_array, idx))
+        cache_train_image_pair.append((image_pair, idx))
         if (len(cache_train_id) == txn_commit_count or idx == len(train_val_split)-1):
             train_string_list = pool.map(image_pair_to_string, cache_train_image_pair)
             with env_train.begin(write=True) as txn_train:
-                for i in range(cache_train_id):
+                for i in range(len(cache_train_id)):
                     txn_train.put(cache_train_id[i], train_string_list[i])
             del cache_train_id[:]
             del cache_train_image_pair[:]
     else:
         cache_val_id.append(key_idx)
-        cache_val_image_pair.append((image_pair, id_to_array))
+        cache_val_image_pair.append((image_pair, idx))
         if (len(cache_val_id) == txn_commit_count or idx == len(train_val_split)-1):
             val_string_list = pool.map(image_pair_to_string, cache_val_image_pair)
             with env_val.begin(write=True) as txn_val:
-                for i in range(cache_val_id):
+                for i in range(len(cache_val_id)):
                     txn_val.put(cache_val_id[i], val_string_list[i])
-            del cache_val_id.clear[:]
+            del cache_val_id[:]
             del cache_val_image_pair[:]
             
     if(idx%report_step == 0):
