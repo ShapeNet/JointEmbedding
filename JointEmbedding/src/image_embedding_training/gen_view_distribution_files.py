64fdf11c2fd45ef80eda887c1a1f6ee95131714d
Yangyan Li <yangyan@orionp.stanford.edu>
2015-09-01 13:40:04 -0700

    Add interface for generating view distribution files.

---
+++ JointEmbedding/src/image_embedding_training/gen_view_distribution_files.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import os, sys
+from random import shuffle
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+sys.path.append(os.path.dirname(BASE_DIR))
+from global_variables import *
+
--- JointEmbedding/src/image_embedding_training/render_syn_shape_list.py
+++ JointEmbedding/src/image_embedding_training/render_syn_shape_list.py
@@ -36,9 +36,12 @@ if __name__ == '__main__':
         shape_synset_images_per_shape[synset] = int(g_images_per_synset / shape_synset_count[synset])
     print('shape_synset_images_per_shape: ', shape_synset_images_per_shape)
 
-    # readin view distribution file
+    # reading view distribution file
     shape_synset_view_params = dict()
     for synset in shape_synset_count:
+        if not os.path.exists(g_view_distribution_files[synset]):
+            print 'Failed to read view distribution files from %s for synset %s'%(g_view_distribution_files[synset], synset)
+            exit()
         view_params = open(g_view_distribution_files[synset]).readlines()
         view_params = [[float(x) for x in line.strip().split(' ')] for line in view_params] 
         shape_synset_view_params[synset] = view_params
