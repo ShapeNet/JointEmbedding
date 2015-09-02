9ecbd822b44765e76542a363a0a5996c02eaeda5
Yangyan Li <yangyan@stanford.edu>
2015-09-01 17:00:58 -0700

    Test one dataset each time as different caffemodel should be used for different datasets.

--- JointEmbedding/src/experiments/evaluation_cross_view_image_retrieval/extract_image_embeddings.py
+++ JointEmbedding/src/experiments/evaluation_cross_view_image_retrieval/extract_image_embeddings.py
@@ -19,6 +19,7 @@ sys.path.append(os.path.dirname(os.path.dirname(BASE_DIR)))
 from global_variables import *
 
 parser = argparse.ArgumentParser(description="Extract image embedding features for IMAGE input.")
+parser.add_argument('--dataset', '-d', help='The dataset for extracting image embedding features(02958343, 03001627_clutter, or 03001627_clean)', required=True)
 parser.add_argument('--iter_num', '-n', help='Use caffemodel trained after iter_num iterations', type=int, default=20000)
 parser.add_argument('--caffemodel', '-c', help='Path to caffemodel (will ignore -n option if provided)', required=False)
 parser.add_argument('--prototxt', '-p', help='Path to prototxt (if not at the default place)', required=False)
@@ -29,7 +30,11 @@ sys.path.append(os.path.join(g_caffe_install_path, 'python'))
 import caffe
 from caffe.proto import caffe_pb2
 
-caffemodel = os.path.join(g_image_embedding_testing_folder, 'snapshots%s_iter_%d.caffemodel'%(g_shapenet_synset_set_handle, args.iter_num))
+dataset_handle = '_'+args.dataset.split('_')[0]
+if len(experiment_name) != 0:
+    dataset_handle = dataset_handle + '_' + experiment_name;
+    
+caffemodel = os.path.join(g_image_embedding_testing_folder, 'snapshots%s_iter_%d.caffemodel'%(dataset_handle, args.iter_num))
 prototxt = g_image_embedding_testing_prototxt
 
 evaluation_folder = os.path.join(g_data_folder, 'evaluation_cross_view_image_retrieval')
@@ -63,28 +68,25 @@ net = caffe.Classifier(prototxt,
                        raw_scale=255,
                        channel_swap=(2, 1, 0))
 
-datasets = ['02958343', '03001627_clutter', '03001627_clean']
+input_data = [None]*batch_size
+filelist = [line.strip() for line in open(os.path.join(evaluation_folder, 'filelist_'+args.dataset+'.txt'))]
+print datetime.datetime.now().time(), 'Computing image embedding for %s (%d images)...'%(args.dataset, len(filelist)) 
 
-for dataset in datasets: 
-    input_data = [None]*batch_size
-    filelist = [line.strip() for line in open(os.path.join(evaluation_folder, 'filelist_'+dataset+'.txt'))]
-    print datetime.datetime.now().time(), 'Computing image embedding for %s (%d images)...'%(dataset, len(filelist)) 
+data_folder = os.path.join(evaluation_folder, 'images_'+args.dataset)
+image_embedding_filename = os.path.join(evaluation_folder, 'image_embeddings_'+args.dataset+'.txt')
+with open(image_embedding_filename, 'w') as image_embedding_file:
+    for idx, filename in enumerate(filelist):
+        input_data[idx%batch_size] = caffe.io.load_image(os.path.join(data_folder, filename))
+        if idx%batch_size == batch_size -1 or idx == len(filelist)-1:
+            net.predict(input_data, oversample=False)
+            image_embedding_blobproto = net.blobs['image_embedding']
+            image_embedding_array = caffe.io.blobproto_to_array(image_embedding_blobproto)
     
-    data_folder = os.path.join(evaluation_folder, 'images_'+dataset)
-    image_embedding_filename = os.path.join(evaluation_folder, 'image_embeddings_'+dataset+'.txt')
-    with open(image_embedding_filename, 'w') as image_embedding_file:
-        for idx, filename in enumerate(filelist):
-            input_data[idx%batch_size] = caffe.io.load_image(os.path.join(data_folder, filename))
-            if idx%batch_size == batch_size -1 or idx == len(filelist)-1:
-                net.predict(input_data, oversample=False)
-                image_embedding_blobproto = net.blobs['image_embedding']
-                image_embedding_array = caffe.io.blobproto_to_array(image_embedding_blobproto)
-        
-                image_embedding_num = batch_size
-                if idx == len(filelist)-1:
-                    image_embedding_num = idx%batch_size + 1
-                print datetime.datetime.now().time(), 'Saving image embedding for images [%d-%d]...'%(idx+1-batch_size, idx)
-                for i in range(image_embedding_num):
-                    image_embedding = image_embedding_array[i, :].reshape(g_shape_embedding_space_dimension).tolist()
-                    image_embedding_file.write(' '.join([str(value) for value in image_embedding])+'\n')
+            image_embedding_num = batch_size
+            if idx == len(filelist)-1:
+                image_embedding_num = idx%batch_size + 1
+            print datetime.datetime.now().time(), 'Saving image embedding for images [%d-%d]...'%(idx+1-batch_size, idx)
+            for i in range(image_embedding_num):
+                image_embedding = image_embedding_array[i, :].reshape(g_shape_embedding_space_dimension).tolist()
+                image_embedding_file.write(' '.join([str(value) for value in image_embedding])+'\n')
     
