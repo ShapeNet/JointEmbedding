44c848c9d724a6b5a9cfe4debb0b8c7a6611050f
Yangyan Li <yangyan@oriong.stanford.edu>
2015-09-04 14:31:55 -0700

    Small changes.

--- src/image_embedding_training/prepare_sun2012_data.py
+++ src/image_embedding_training/prepare_sun2012_data.py
@@ -16,12 +16,10 @@ if not os.path.exists(g_sun2012_data_folder):
 # download and unzip SUN2012 data
 filename = os.path.join(g_sun2012_data_folder, '../SUN2012pascalformat.tar.gz')
 if os.path.exists(filename):
-    if query_yes_no(('It seems that you have downloaded SUN2012 data to \"%s\", skip downloading?')%(filename)):
-        exit()
-    else:
+    if not query_yes_no(('It seems that you have downloaded SUN2012 data to \"%s\", skip downloading?')%(filename)):
         os.remove(filename)
+        call(['wget', '-O', filename, g_sun2012_data_url])
 
-call(['wget', '-O', filename, g_sun2012_data_url])
 call(['tar', 'xvzf', filename, '-C', g_sun2012_data_folder, '--strip-components=1'])
 filelist = open(g_syn_bkg_filelist, 'w')
 for filename in sorted(os.listdir(g_syn_bkg_folder)):
