61c70ddbd63507d33318fd890af0decb091189c0
Yangyan Li <yangyan@orionp.stanford.edu>
2015-09-07 02:00:41 -0700

    Small fixes

--- src/image_embedding_training/convert_lmdb_imageid2shapeid.py
+++ src/image_embedding_training/convert_lmdb_imageid2shapeid.py
@@ -6,11 +6,14 @@ import sys
 import lmdb
 import shutil
 import datetime
+import argparse
 from multiprocessing import Pool
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 sys.path.append(os.path.dirname(BASE_DIR))
 from global_variables import *
+sys.path.append(os.path.join(g_caffe_install_path, 'python'))
+import caffe
 try:
     # Python3 will most likely not be able to load protobuf
     from caffe.proto import caffe_pb2
@@ -24,15 +27,16 @@ parser = argparse.ArgumentParser(description="Convert imageid in LMDB into shape
 parser.add_argument('--input_lmdb', help='Path to input LMDB.', required=True)
 parser.add_argument('--output_lmdb', help='Path to output LMDB.', required=True)
 parser.add_argument('--imageid2shapeid', help='Path to imageid2shapeid file.', required=True)
+args = parser.parse_args()
 
 print 'Loading imageid2shapeid file from %s...'%(args.imageid2shapeid)
 imageid2shapeid_mapping = [int(line.strip()) for line in open(args.imageid2shapeid, 'r')]
         
 def convert_imageid2shapeid(datum_string, def_param=imageid2shapeid_mapping):
-    assert(imageid_output != -1)
     datum = caffe_pb2.Datum()
     datum.ParseFromString(datum_string)
     datum.label = imageid2shapeid_mapping[datum.label]
+    assert(datum.label != -1)
     return datum.SerializeToString()
 
 pool = Pool(g_extract_feat_thread_num)
@@ -44,17 +48,16 @@ env_output = lmdb.open(args.output_lmdb, map_size=int(1e12))
 
 txn_commit_count = 512
 report_step = 10000;
-idx = 0
+image_count = 0
 cache_key_output = []
 cache_value_input = []
 with env_input.begin() as txn:
     cursor = txn.cursor()
     for key, value in cursor:
-        key_output = '{:0>10d}'.format(idx)
-        assert(key == key_output)
+        key_output = '{:0>10d}'.format(image_count)
         cache_key_output.append(key_output)
         cache_value_input.append(value)
-        if (len(cache_key_output) == txn_commit_count or idx == len(imageid2shapeid_mapping)-1):
+        if (len(cache_key_output) == txn_commit_count or image_count == len(imageid2shapeid_mapping)-1):
             cache_value_output = pool.map(convert_imageid2shapeid, cache_value_input)
             with env_output.begin(write=True) as txn_output:
                 for idx in range(len(cache_key_output)):
@@ -62,6 +65,6 @@ with env_input.begin() as txn:
             del cache_key_output[:]
             del cache_value_input[:]
             
-        if(idx%report_step == 0):
-            print datetime.datetime.now().time(), '-', idx, 'of', len(imageid2shapeid_mapping), 'processed!'
-        idx = idx + 1
+        if(image_count%report_step == 0):
+            print datetime.datetime.now().time(), '-', image_count, 'of', len(imageid2shapeid_mapping), 'processed!'
+        image_count = image_count + 1
