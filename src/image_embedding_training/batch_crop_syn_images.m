e0da792e0a94b8658aeed09f87925d5d2702f29f
Yangyan Li <yangyan.li@hotmail.com>
2015-09-29 12:03:07 -0700

    Update batch_crop_syn_images.m

--- src/image_embedding_training/batch_crop_syn_images.m
+++ src/image_embedding_training/batch_crop_syn_images.m
@@ -8,7 +8,7 @@ image_list = collect_image_list(g_syn_images_folder, g_shape_list_file);
 local_cluster = parcluster('local');
 poolobj = parpool('local', min(g_syn_cropping_thread_num, local_cluster.NumWorkers));
 fprintf('Batch cropping synthetic images from \"%s\" to \"%s\" ...\n', g_syn_images_folder, g_syn_images_cropped_folder);
-cropRatios = [0.05,0.3,0.05,0.05];
+cropRatios = [0.05,0.05,0.05,0.05];
 batch_crop(g_syn_images_folder, g_syn_images_cropped_folder, 1, image_list, cropRatios);
 delete(poolobj);
 
