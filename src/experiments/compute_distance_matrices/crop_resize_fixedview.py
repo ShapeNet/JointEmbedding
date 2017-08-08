3a5fd0d00234cfa1441f890f8bfcc66d95ad714b
Tang Lee <weitang114@gmail.com>
2017-08-08 15:22:37 +0800

    fix argument of mogrify command

--- src/experiments/compute_distance_matrices/crop_resize_fixedview.py
+++ src/experiments/compute_distance_matrices/crop_resize_fixedview.py
@@ -5,4 +5,4 @@ sys.path.append(os.path.dirname(os.path.dirname(BASE_DIR)))
 from global_variables import *
 
 # parallely crop and resize images
-os.system("find %s -name '*.png'|xargs -n 1 -P 40 mogrify -trim -resize '227*227!'" % (g_fixedview_image_folder))
+os.system("find %s -name '*.png'|xargs -n 1 -P 40 mogrify -trim -resize '227x227!'" % (g_fixedview_image_folder))
